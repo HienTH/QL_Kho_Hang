088424e94df0701bf371753757f95957ae6c4fb8
phamngocanh151195 <0ongocanh0ovt12@gmail.com>
2016-11-23 22:57:22 +0700

    Form NCC + Nhập

--- QL_KhoHang/QL_KhoHang/frmNCC.Designer.cs
+++ QL_KhoHang/QL_KhoHang/frmNCC.Designer.cs
@@ -28,19 +28,206 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.dtgrvNCC = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMaNCC = new System.Windows.Forms.TextBox();
+            this.txtTenNCC = new System.Windows.Forms.TextBox();
+            this.txtDCNCC = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.thêmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sửaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xóaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lưuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtSDTNCC = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrvNCC)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(587, 30);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 0;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // dtgrvNCC
+            // 
+            this.dtgrvNCC.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dtgrvNCC.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgrvNCC.Location = new System.Drawing.Point(121, 150);
+            this.dtgrvNCC.Name = "dtgrvNCC";
+            this.dtgrvNCC.Size = new System.Drawing.Size(466, 268);
+            this.dtgrvNCC.TabIndex = 2;
+            this.dtgrvNCC.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgrvNCC_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(221, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Danh Sách Nhà Cung Cấp";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Mã";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(389, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Địa Chỉ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(118, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Tên";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(389, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "SĐT";
+            // 
+            // txtMaNCC
+            // 
+            this.txtMaNCC.Location = new System.Drawing.Point(171, 63);
+            this.txtMaNCC.Name = "txtMaNCC";
+            this.txtMaNCC.Size = new System.Drawing.Size(140, 20);
+            this.txtMaNCC.TabIndex = 13;
+            // 
+            // txtTenNCC
+            // 
+            this.txtTenNCC.Location = new System.Drawing.Point(171, 99);
+            this.txtTenNCC.Name = "txtTenNCC";
+            this.txtTenNCC.Size = new System.Drawing.Size(140, 20);
+            this.txtTenNCC.TabIndex = 14;
+            // 
+            // txtDCNCC
+            // 
+            this.txtDCNCC.Location = new System.Drawing.Point(447, 63);
+            this.txtDCNCC.Name = "txtDCNCC";
+            this.txtDCNCC.Size = new System.Drawing.Size(140, 20);
+            this.txtDCNCC.TabIndex = 15;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.thêmToolStripMenuItem,
+            this.sửaToolStripMenuItem,
+            this.xóaToolStripMenuItem,
+            this.lưuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(744, 24);
+            this.menuStrip1.TabIndex = 16;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // thêmToolStripMenuItem
+            // 
+            this.thêmToolStripMenuItem.Name = "thêmToolStripMenuItem";
+            this.thêmToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.thêmToolStripMenuItem.Text = "Thêm";
+            this.thêmToolStripMenuItem.Click += new System.EventHandler(this.thêmToolStripMenuItem_Click_1);
+            // 
+            // sửaToolStripMenuItem
+            // 
+            this.sửaToolStripMenuItem.Name = "sửaToolStripMenuItem";
+            this.sửaToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.sửaToolStripMenuItem.Text = "Sửa";
+            this.sửaToolStripMenuItem.Click += new System.EventHandler(this.sửaToolStripMenuItem_Click);
+            // 
+            // xóaToolStripMenuItem
+            // 
+            this.xóaToolStripMenuItem.Name = "xóaToolStripMenuItem";
+            this.xóaToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.xóaToolStripMenuItem.Text = "Xóa";
+            this.xóaToolStripMenuItem.Click += new System.EventHandler(this.xóaToolStripMenuItem_Click);
+            // 
+            // lưuToolStripMenuItem
+            // 
+            this.lưuToolStripMenuItem.Name = "lưuToolStripMenuItem";
+            this.lưuToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.lưuToolStripMenuItem.Text = "Lưu";
+            this.lưuToolStripMenuItem.Click += new System.EventHandler(this.lưuToolStripMenuItem_Click);
+            // 
+            // txtSDTNCC
+            // 
+            this.txtSDTNCC.Location = new System.Drawing.Point(447, 99);
+            this.txtSDTNCC.Name = "txtSDTNCC";
+            this.txtSDTNCC.Size = new System.Drawing.Size(140, 20);
+            this.txtSDTNCC.TabIndex = 17;
             // 
             // frmNCC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(744, 404);
+            this.Controls.Add(this.txtSDTNCC);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.txtDCNCC);
+            this.Controls.Add(this.txtTenNCC);
+            this.Controls.Add(this.txtMaNCC);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtgrvNCC);
+            this.Controls.Add(this.btnThoat);
             this.Name = "frmNCC";
             this.Text = "Nhà Cung Cấp";
+            this.Load += new System.EventHandler(this.frmNCC_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrvNCC)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.DataGridView dtgrvNCC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMaNCC;
+        private System.Windows.Forms.TextBox txtTenNCC;
+        private System.Windows.Forms.TextBox txtDCNCC;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem thêmToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sửaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xóaToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtSDTNCC;
+        private System.Windows.Forms.ToolStripMenuItem lưuToolStripMenuItem;
     }
 }
--- QL_KhoHang/QL_KhoHang/frmNhap.cs
+++ QL_KhoHang/QL_KhoHang/frmNhap.cs
@@ -12,9 +12,213 @@ namespace QL_KhoHang
 {
     public partial class frmNhap : Form
     {
+        KetNoiCSDL kn = new KetNoiCSDL();
+        int i = 0;
         public frmNhap()
         {
             InitializeComponent();
+        }
+        public void Trong()
+        {
+            txtTenHH.Text = "";
+            txtThongTin.Text = "";
+            txtNSX.Text = "";
+            txtGN.Text = "";
+            txtSL.Text = "";
+        }
+        public void KhoaButton()
+        {
+            btnThem.Enabled = false;
+            btnLuu.Enabled = false;
+            button2.Enabled = false;
+        }
+        public void MoKhoaButton()
+        {
+            btnThem.Enabled = true;
+            btnLuu.Enabled = true;
+            button2.Enabled = true;
+        }
+        public void Khoa()
+        {
+            txtTenHH.Enabled = false;
+            txtThongTin.Enabled = false;
+            txtNSX.Enabled = false;
+            txtGN.Enabled = false;
+            txtSL.Enabled = false;
+        }
+        public void MoKhoa()
+        {
+            txtTenHH.Enabled = true;
+            txtThongTin.Enabled = true;
+            txtNSX.Enabled = true;
+            txtGN.Enabled = true;
+            txtSL.Enabled = true;
+        }
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            string sql = "select * from HANGHOA";
+            dtgrvHH.DataSource = kn.Get(sql);
+            MoKhoa();
+            i = 1;
+            string chuoi = "";
+            int so = 0;
+            chuoi = Convert.ToString(dtgrvHH.Rows[dtgrvHH.RowCount - 2].Cells[0].Value);
+            chuoi = chuoi.Remove(0, 3);
+            so = Convert.ToInt32(chuoi);
+            if (so + 1 < 10)
+            {
+                chuoi = "HH0000000" + Convert.ToString(so + 1);
+                txtMaHH.Text = chuoi;
+            }
+            else if (so + 1 >= 10)
+            {
+                chuoi = "HH000000" + Convert.ToString(so + 1);
+                txtMaHH.Text = chuoi;
+            }
+            Trong();
+            string sql2 = "select * from HANGHOA where MaHH = (select CHITIETPHIEUNHAP.MaHH from CHITIETPHIEUNHAP where CHITIETPHIEUNHAP.MaPN = '" + txtPDN.Text + "')";
+            dtgrvHH.DataSource = kn.Get(sql2);
+        }
+        private void frmNhap_Load(object sender, EventArgs e)
+        {
+            KhoaButton();
+
+            Khoa();
+            btnLuuPN.Enabled = false;
+
+            dateTimePicker1.Enabled = false;
+            dateTimePicker1.Value = DateTime.Today; 
+            txtMaPN.Enabled = false;
+            txtPDN.Enabled = false;
+            cboTenNCC.Enabled = true;
+            btnTaoMoi.Enabled = true;
+
+            string sql = "select * from PHIEUNHAP";
+            dtgrvPN.DataSource = kn.Get(sql);
+
+            string sql1 = "select * from HANGHOA";
+            dtgrvHH.DataSource = kn.Get(sql1);
+
+            string sql3 = "select * from NHACUNGCAP";
+            cboTenNCC.DataSource = kn.Get(sql3);
+            cboTenNCC.ValueMember = "MaNCC";
+            cboTenNCC.DisplayMember = "TenNCC";
+
+            cboTenNCC.Text = "";
+        }
+        private void btnXong_Click(object sender, EventArgs e)
+        {
+            txtMaPN.Text = "";
+            txtPDN.Text = "";
+            txtPDN.Text = "";
+            cboTenNCC.Text = "";
+            btnLuuPN.Enabled = false;
+            btnTaoMoi.Enabled = true;
+            string sql = "select * from HANGHOA";
+            dtgrvHH.DataSource = kn.Get(sql);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dlr = MessageBox.Show("Bạn có chắc chắn muốn hủy phiếu đang nhập???", "Hủy Phiếu Nhập", MessageBoxButtons.OKCancel,MessageBoxIcon.Question);
+            if(dlr==DialogResult.OK)
+            {
+                string view = "alter View THH as select MaHH from CHITIETPHIEUNHAP where MaPN='" + txtPDN.Text + "'";
+                string sql2 = "delete from CHITIETPHIEUNHAP where MaPN='" + txtPDN.Text + "'";
+                kn.Get(sql2);
+                string sql = "delete from PHIEUNHAP where MaPN='"+ txtPDN.Text +"'";
+                kn.Get(sql);
+                string sql3 = "delete from HANGHOA where MaHH = (select MaHH from THH)";
+                kn.Get(sql3);
+                string sql1 = "select * from PHIEUNHAP";
+                dtgrvPN.DataSource = kn.Get(sql1);
+            }
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            if(i==1)
+            {
+                int x=int.Parse(txtGN.Text);
+                int y=int.Parse(txtSL.Text);
+                
+                string sql1 = "insert into HANGHOA values('" + txtMaHH.Text + "',N'" + txtTenHH.Text + "','" + txtSL.Text + "','" + txtGN.Text + "',N'" + txtNSX.Text + "',N'" + txtThongTin.Text + "')";
+                kn.Get(sql1);
+
+                string sql = "insert into CHITIETPHIEUNHAP values('" + txtPDN.Text + "','" + txtMaHH.Text + "','" + txtSL.Text + "','" + txtGN.Text + "','" + x * y + "')";
+                kn.Get(sql);                
+                dtgrvHH.DataSource = kn.Get("select * from HANGHOA");
+
+                string sql2 = "update PHIEUNHAP set TongTien=TongTien+'" + x * y + "' where MaPN='" + txtPDN.Text + "'";
+                dtgrvPN.DataSource = kn.Get(sql2);
+                
+                dtgrvPN.DataSource = kn.Get("select * from PHIEUNHAP");
+                Trong();
+
+                string sql3 = "select * from HANGHOA where MaHH in (select CHITIETPHIEUNHAP.MaHH from CHITIETPHIEUNHAP where CHITIETPHIEUNHAP.MaPN = '" + txtPDN.Text + "')";
+                dtgrvHH.DataSource = kn.Get(sql3);
+
+                txtMaHH.Text = "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Trong();
+            txtMaHH.Text = "";
+            Khoa();
+        }
+
+        private void btnLuuPN_Click_1(object sender, EventArgs e)
+        {
+            if (txtMaPN.Text != "" && cboTenNCC.Text != "")
+            {
+                string sql1 = "select * from PHIEUNHAP";
+                string sql = "insert into PHIEUNHAP values('" + txtMaPN.Text + "',N'" + cboTenNCC.SelectedValue + "',N'" + dateTimePicker1.Value + "','0')";
+                kn.Get(sql);
+                dtgrvPN.DataSource = kn.Get(sql1);
+
+                string sql2 = "select * from HANGHOA where MaHH = (select CHITIETPHIEUNHAP.MaHH from CHITIETPHIEUNHAP where CHITIETPHIEUNHAP.MaPN = '" + txtPDN.Text + "')";
+                dtgrvHH.DataSource = kn.Get(sql2);
+
+                txtPDN.Text = txtMaPN.Text;
+                cboTenNCC.Enabled = false;
+                btnTaoMoi.Enabled = false;
+                btnLuuPN.Enabled = false;
+
+                MoKhoaButton();
+            }
+        }
+
+        private void btnTaoMoi_Click_1(object sender, EventArgs e)
+        {
+            btnLuuPN.Enabled = true;
+            cboTenNCC.Text = "";
+            string sql = "select * from PHIEUNHAP";
+            dtgrvPN.DataSource = kn.Get(sql);
+
+            string chuoi = "";
+            int so = 0;
+            chuoi = Convert.ToString(dtgrvPN.Rows[dtgrvPN.RowCount - 2].Cells[0].Value);
+            chuoi = chuoi.Remove(0, 3);
+            so = Convert.ToInt32(chuoi);
+            if (so + 1 < 100)
+            {
+                chuoi = "PN00" + Convert.ToString(so + 1);
+                txtMaPN.Text = chuoi;
+            }
+            else if (so + 1 >= 100)
+            {
+                chuoi = "PN0" + Convert.ToString(so + 1);
+                txtMaPN.Text = chuoi;
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            string sql = "delete from PHIEUNHAP where TongTien=" + 0;
+            kn.Get(sql);
+            this.Close();
         }
     }
 }
